fea507f61475de32a0778a9d57514bb98c8112f6
PanovValerii <genadynikur@gmail.com>
2024-10-21 18:28:21 +0300

    Add files via upload

---
+++ Панов_Валерий_Минидомашка1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int square(int x) {
+    return x * x;
+}
+
+double trapeze(double a, double b, double h) {
+    return (h * (a + b)) / 2.0;
+}
+
+int fibbonachi(int N) {
+    if (N < 3) {
+        return 1;
+    }
+    else {
+        return fibbonachi(int(N - 2)) + fibbonachi(int(N - 1));
+    }
+}
+
+void cba(int x) {
+    while (x != 0) {
+        cout << x % 10;
+        x /= 10;
+    }
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << square(x);
+    float a;
+    float b;
+    float h;
+    cin >> a;
+    cin >> b;
+    cin >> h;
+    cout << trapeze(a, b, h);
+    int N;
+    cin >> N;
+    for (int i = 1; i < N + 1; i++) {
+        cout << fibbonachi(i) << " ";
+    }
+    int k;
+    cin >> k;
+    cba(k);
+    cin.get();
+}
---
+++ Панов_Валерий_Минидомашка2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string oct_hex(int x, int y) {
+    string s = " ";
+    while (x != 0) {
+        if (x % y > 9) {
+            char k = x % y + 55;
+            s = k + s;
+        }
+        else {
+            char k = x % y + 48;
+            s = k + s;
+        }
+        x /= y;
+    }
+    return s;
+}
+
+int Sirakus(int x) {
+    if (x % 2 == 0) {
+        x /= 2;
+    }
+    else {
+        x *= 3;
+        x++;
+        x /= 2;
+    }
+    return x;
+}
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << oct_hex(x, y);
+    int n;
+    cin >> n;
+    while (n != 1) {
+        n = Sirakus(n);
+        if (n == 1) {
+            cout << "True";
+        }
+    }
+    cin.get();
+}
+
